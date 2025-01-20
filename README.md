c9bde7b34c21551c74f72d63a6a8eddd61251342
mitama <loligothick@gmail.com>
2025-01-21 03:24:36 +0900

    feat: _i1

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# unindent
+# Unindent
 
-This is a small C++20 library that provides a simple way to unindent the raw string literals at compile time.
+This is a small C++20 library that provides a simple way to make indent-adjusted multiline raw string literals at compile time.
 
-## Example
+## Synopsis
+
+Unindent provides two new user-defined literals for indented strings: `"..."_i` and `"..."_i1`:
+
+The `"..."_i` UDL removes the indent applied in the source file. The behaviour is similar to Coffeescript's [multiline string literals](https://coffeescript.org/#strings):
 
 ```cpp
-#include <iostream>
 #include <string_view>
 
 #include <unindent/unindent.hpp>
@@ -20,7 +23,31 @@ int main() {
       print("World")
   )"_i;
 
-  static_assert(unindented_str == "def foo():\n  print(\"Hello\")\n  print(\"World\")"sv);
+  static_assert(unindented_str ==
+                "def foo():\n  print(\"Hello\")\n\n  print(\"World\")"sv);
 }
 ```
 
+The `"..."_i1` UDL is like `"..."_i`, except it folds "paragraphs" of text into single lines. The behaviour is similar to [YAML's folded multiline strings](https://yaml.org/spec/1.2-old/spec.html#id2796251):
+
+```cpp
+#include <string_view>
+
+#include <unindent/unindent.hpp>
+
+int main() {
+  using namespace std::literals;
+  using namespace mitama::unindent::literals;
+  constexpr std::string_view folded_str = R"(
+    This is the first line.
+    This line is appended to the first.
+
+    This line follows a line break.
+      This line ends up indented by two spaces.
+  )"_i1;
+
+  static_assert(
+      folded_str ==
+      "This is the first line. This line is appended to the first.\nThis line follows a line break.   This line ends up indented by two spaces."sv);
+}
+```
--- include/unindent/unindent.hpp
+++ include/unindent/unindent.hpp
@@ -12,7 +12,10 @@
 #include <utility>
 
 namespace mitama::unindent {
-template <std::size_t N, class CharT> struct fixed_string {
+
+namespace details {
+// fixed_string (structural type)
+template <class CharT, std::size_t N> struct fixed_string {
   static constexpr std::size_t size = N;
   using char_type = CharT;
 
@@ -25,96 +28,111 @@ template <std::size_t N, class CharT> struct fixed_string {
   const std::array<CharT, N> s;
 };
 
-template <std::size_t N, class CharT>
-std::ostream &operator<<(std::ostream &os, fixed_string<N, CharT> fs) {
+template <class CharT, std::size_t N>
+inline std::ostream &operator<<(std::ostream &os, fixed_string<CharT, N> fs) {
   return os << fs.s;
 }
+} // namespace details
 
-// non-type template enabled static string class
-//
-// S: fixed_string<N, CharT>
-template <fixed_string S> struct static_string {
-  using char_type = typename decltype(S)::char_type;
-  static constexpr const std::basic_string_view<char_type> value = {
-      S.s, decltype(S)::size};
+namespace details {
+template <class CharT, std::size_t N>
+consteval auto to_unindented(std::array<CharT, N> raw) {
+  namespace views = std::ranges::views;
+  using namespace std::literals;
+  using std::size_t;
+  auto str = std::basic_string_view<CharT>(raw.data());
 
-  template <auto T>
-    requires std::same_as<char_type, typename static_string<T>::char_type>
-  constexpr std::strong_ordering operator<=>(static_string<T>) const noexcept {
-    return static_string::value <=> static_string<T>::value;
+  while (str.ends_with(' '))
+    str.remove_suffix(1);
+  while (str.ends_with('\n'))
+    str.remove_suffix(1);
+  while (str.starts_with('\n'))
+    str.remove_prefix(1);
+
+  auto is_space = [](CharT c) { return c == ' '; };
+
+  auto lines = str | views::split("\n"sv);
+  auto lines_without_empty =
+      str | views::split("\n"sv) |
+      views::filter([](auto line) { return !line.empty(); });
+
+  auto min_indent = std::numeric_limits<size_t>::max();
+
+  std::array<CharT, N> buffer = {};
+  size_t index = 0;
+
+  for (auto line : lines_without_empty) {
+    auto iter = std::find_if_not(line.begin(), line.end(), is_space);
+    min_indent = std::min(
+        {min_indent, static_cast<size_t>(std::distance(line.begin(), iter))});
   }
 
-  constexpr operator std::basic_string_view<char_type>() const { return value; }
-};
-
-template <auto S> std::ostream &operator<<(std::ostream &os, static_string<S>) {
-  return os << static_string<S>::value;
-}
-
-template <fixed_string S> class unindented {
-public:
-  using char_type = decltype(S)::char_type;
-
-private:
-  static consteval std::array<char_type, decltype(S)::size>
-  cvt(std::array<char_type, decltype(S)::size> raw) {
-    namespace views = std::ranges::views;
-    using namespace std::literals;
-    using std::size_t;
-    auto str = std::basic_string_view<char_type>(raw.data());
-
-    while (str.ends_with(' '))
-      str.remove_suffix(1);
-    while (str.starts_with('\n'))
-      str.remove_prefix(1);
-
-    auto is_space = [](char_type c) { return c == ' '; };
-
-    auto lines = str | views::split("\n"sv) | views::filter([](auto line) {
-      return !line.empty();
-    });
-
-    auto min_indent = std::numeric_limits<size_t>::max();
-
-    std::array<char_type, decltype(S)::size> buffer = {};
-    size_t index = 0;
-
-    for (auto line : lines) {
-      auto iter = std::find_if_not(line.begin(), line.end(), is_space);
-      min_indent = std::min({min_indent, static_cast<size_t>(std::distance(line.begin(), iter))});
+  if (min_indent == std::numeric_limits<size_t>::max() || min_indent == 0) {
+    for (auto c : str) {
+      buffer[index++] = c;
     }
-
-    if (min_indent == std::numeric_limits<size_t>::max() || min_indent == 0) {
-      for (auto c : str) {
-        buffer[index++] = c;
-      }
-      return buffer;
-    }
-
-    for (auto line : lines | views::transform([min_indent](auto line) {
-                    std::basic_string_view<char_type> line_view{line.begin(),
-                                                                line.end()};
-                    line_view.remove_prefix(min_indent);
-                    return line_view;
-                  })) {
-      for (auto c : line) {
-        buffer[index++] = c;
-      }
-      buffer[index++] = '\n';
-    }
-    buffer[index - 1] = '\0';
     return buffer;
   }
 
+  for (auto line : lines | views::transform([min_indent](auto line) {
+                     std::basic_string_view<CharT> line_view{line.begin(),
+                                                             line.end()};
+                     if (line_view.size() < min_indent)
+                       return line_view;
+                     line_view.remove_prefix(min_indent);
+                     return line_view;
+                   })) {
+    for (auto c : line) {
+      buffer[index++] = c;
+    }
+    buffer[index++] = '\n';
+  }
+  buffer[index - 1] = '\0';
+  return buffer;
+}
+
+template <class CharT, std::size_t N>
+consteval auto to_folded(std::array<CharT, N> raw) {
+  namespace views = std::ranges::views;
+
+  auto is_return = [](CharT c) { return c == '\n'; };
+
+  std::array<CharT, N> buffer = {};
+  size_t index = 0;
+  size_t returns = 0;
+
+  for (auto c : to_unindented(raw)) {
+    if (is_return(c)) {
+      returns++;
+    } else {
+      if (returns > 1) {
+        buffer[index++] = '\n';
+      } else if (returns == 1) {
+        buffer[index++] = ' ';
+      }
+      buffer[index++] = c;
+      returns = 0;
+    }
+  }
+
+  return buffer;
+}
+} // namespace details
+
+// unindented string (value type)
+template <details::fixed_string Lit> class unindented {
+public:
+  using char_type = decltype(Lit)::char_type;
+
 private:
-  static constexpr std::array<char_type, decltype(S)::size> raw_ = S.s;
-  static constexpr std::array<char_type, decltype(S)::size> value_ = cvt(S.s);
+  static constexpr std::array<char_type, decltype(Lit)::size> value_ =
+      details::to_unindented(Lit.s);
 
 public:
-  template <auto T>
-    requires std::same_as<char_type, typename unindented<T>::char_type>
-  constexpr std::strong_ordering operator<=>(unindented<T>) const noexcept {
-    return unindented::value_ <=> unindented<T>::value_;
+  template <auto S>
+    requires std::same_as<char_type, typename unindented<S>::char_type>
+  constexpr std::strong_ordering operator<=>(unindented<S>) const noexcept {
+    return unindented::value_ <=> unindented<S>::value_;
   }
 
   constexpr operator std::basic_string_view<char_type>() const {
@@ -125,12 +143,51 @@ public:
   }
 };
 
-template <fixed_string S>
-std::ostream &operator<<(std::ostream &os, unindented<S>) {
+template <details::fixed_string S>
+inline std::ostream &operator<<(std::ostream &os, unindented<S>) {
   return os << unindented<S>::value();
 }
 
-template <fixed_string S> inline constexpr unindented<S> operator""_i() {
+// folded string (value type)
+template <details::fixed_string Lit> class folded {
+public:
+  using char_type = decltype(Lit)::char_type;
+
+private:
+  static constexpr std::array<char_type, decltype(Lit)::size> value_ =
+      details::to_folded(Lit.s);
+
+public:
+  template <auto S>
+    requires std::same_as<char_type, typename folded<S>::char_type>
+  constexpr std::strong_ordering operator<=>(folded<S>) const noexcept {
+    return folded::value_ <=> folded<S>::value_;
+  }
+
+  constexpr operator std::basic_string_view<char_type>() const {
+    return std::basic_string_view<char_type>(value_.data());
+  }
+  static constexpr std::basic_string_view<char_type> value() {
+    return std::basic_string_view<char_type>(value_.data());
+  }
+};
+
+template <details::fixed_string S>
+inline std::ostream &operator<<(std::ostream &os, folded<S>) {
+  return os << folded<S>::value();
+}
+
+} // namespace mitama::unindent
+
+namespace mitama::unindent::inline literals {
+
+template <details::fixed_string S>
+inline constexpr unindented<S> operator""_i() {
   return {};
 }
-} // namespace mitama::unindent
+
+template <details::fixed_string S> inline constexpr folded<S> operator""_i1() {
+  return {};
+}
+
+} // namespace mitama::unindent::inline literals
